0238f914b78c402a237b3c73ba8b0aaa3d9476fc
Anup Joseph <anup20joseph@gmail.com>
2020-02-20 12:06:30 +0530

    Completed Vernam Cypher

--- CSS/verman.py
+++ CSS/verman.py
@@ -1,16 +1,18 @@
-import pdb
-messsage = [ord(i) - 96 if i >= 'a' else ord(i) - 64 for i in input("\nEnter the message to be encrypted\n") ]
-key = [ord(i) - 96 if i >= 'a' else ord(i) - 64 for i in input("\nEnter the key \n") ]
+plain_text = [ ord(i) - 96 if i>= 'a' else ord(i) - 64 for i in input("Enter plain text") ] 
+key_text = [ ord(i) - 96 if i>= 'a' else ord(i) - 64 for i in input("Enter plain text") ]
 
-if len(key) <= len(messsage):
-    remainder = len(messsage) % len(key)
-    temp = [messsage.append(0) for i in range(remainder) ]
-    factor = len(messsage)//len(key) 
-i,j = 0,len(key)
-cypher = []
-while i < len(messsage):
-    temp = messsage[i:i+len(key)]
-    cypher.append([ (temp[x]+key[x])%26 for x in range(len(key))])
-    i = i + len(key)
-    pdb.set_trace()
-print(cypher)
+#Key Building
+factor = len(plain_text)//len(key_text) + 1
+key = []
+for i in range(factor):
+    for j in range(len(key_text)):
+        key.append(key_text[j])
+key = key[:len(plain_text)]
+
+#Verman Encoding
+cypher_text = [ plain_text[i] + key[i] for i in range(len(plain_text)) ]
+cypher_text = [ cypher_text[i] - 26 if cypher_text[i]>26 else cypher_text[i] for i in range(len(cypher_text))]
+cypher_text = [ cypher_text[i] - 1 for i in range(len(cypher_text)) ]
+print("Message after vernam encoding {}".format(cypher_text))
+
+tempkey = sorted(key)

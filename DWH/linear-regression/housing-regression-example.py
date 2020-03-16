d8eb3b382ebe4141991b7bd03a6c3029f0b4bbb6
FenerRevee <doryforosdoryforos@gmail.com>
2020-03-16 10:43:21 +0530

    First Iteration Completed

--- DWH/linear-regression/housing-regression-example.py
+++ DWH/linear-regression/housing-regression-example.py
@@ -1,3 +1,11 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 dataset = pd.read_csv('housing.csv')
+
+X = pd.DataFrame(dataset.iloc[:,:-1])
+Y = pd.DataFrame(dataset.iloc[:,-1])
+X_train,X_test,Y_train,Y_test = train_test_split(test_size=0.2,random_state=0.5)
+regressor = LinearRegression()
+regressor.fit(X_train,Y_train)

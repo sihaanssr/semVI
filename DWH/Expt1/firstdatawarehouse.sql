5214fc5879947de7b25b764165eeaa67ee44b8c8
vedantsahai18 <vedantsahai18@gmail.com>
2020-03-18 23:42:58 +0530

    date warehouse code

--- DWH/Expt1/firstdatawarehouse.sql
+++ DWH/Expt1/firstdatawarehouse.sql
@@ -1,50 +1,68 @@
-CREATE TABLE customerTable(
-	customerId SERIAL PRIMARY KEY,
-	customerName VARCHAR(30),
-	customerEmail VARCHAR(50),
-	DistanceCovered NUMERIC(10)
+CREATE TABLE dimproduct(
+	pid VARCHAR(30) PRIMARY KEY,
+	pkey VARCHAR(30),
+	pname VARCHAR(50),
+	unitprice NUMERIC(10),
+    category VARCHAR(30),
 );
-INSERT INTO customerTable
-	(customerName,customerEmail,DistanceCovered)
+INSERT INTO dimproduct
+	(pid, pkey, pname, unitprice, category)
 VALUES
-	('Bobby Charlton','bobbycharl@ymail.com',3000),
-	('Dennis Law','dennisthemenance@outlook.com',2000),
-	('George Best','bestisbest@quixote.com',10000),
-	('Roy Keane','keanoroy@hotmail.com',2000),
-	('Paul Scholes','scholsey@gmail.com',12000),
-	('Ryan Giggs','ryangiggs@gmail.com',15000),
-	('Nemanja Vidic','vidicnemanja@gmail.com',10000);
+	('1','1001','lg',3000,'phone'),
+	('2','1001','lg',2000,'washingmachine'),
+	('3','1002','samsung',10000,'phone'),
+	('4','1003','apple',2000,'laptop'),
+	('5','1004','nokia',12000,'phone');
 
+CREATE TABLE dimtimetable(
+	tid VARCHAR(30) PRIMARY KEY,
+	tkey VARCHAR(30),
+	day VARCHAR(50),
+	month NUMERIC(10),
+    quarter NUMERIC(10),
+    year NUMERIC(10),
 
-CREATE TABLE driverTable(
-	driverId SERIAL PRIMARY KEY,
-	driverName VARCHAR(30),
-	vehicleName VARCHAR(50),
-	totalDistance NUMERIC(10)
 );
-INSERT INTO driverTable
-	(driverName,vehicleName,totalDistance)
+INSERT INTO dimtimetable
+	(tid, tkey, day, month, quarter, year)
 VALUES
-	('Sir Matt Busby','Mercedes',30000),
-	('Jimmy Murphy','Ford',25000),
-	('Sir Alex Ferguson','Aston Martin',100000),
-	('David McGill','Toyota',20000);
+	('12','1111','Monday',3,2,2018),
+	('34','2222','Tuesday',2,3,2019),
+	('56','3333','Wednesday',1,4,2020),
+	('78','4444','Friday',4,1,2018),
+    ('90','5555','Sunday',11,2,2017);
 
-CREATE TABLE locationTable(
-	locationId SERIAL PRIMARY KEY,
-	addressloc VARCHAR(30),
+CREATE TABLE dimstore(
+	sid VARCHAR(30) PRIMARY KEY,
+    skey VARCHAR(30),
+	sname VARCHAR(30),
 	city VARCHAR(30),
+    state VARCHAR(30),
 	country VARCHAR(30),
 	region VARCHAR(30)
 );
 
-INSERT INTO locationtable
-	(addressloc, city, country, region)
+INSERT INTO dimstore
+	(sid, skey, sname, city, state, county, region)
 VALUES
-	('Stretford','Manchester','Great Britain','Europe'),
-	('Tottenham','London','Great Britain','Europe'),
-	('Carlsyle','Edinburgh','Scotland','Europe'),
-	('Abaroska','Dakota','United States of America','America'),
-	('Tianmenn','Beijing','China','Asia');
+	('11','1234','Stretford','Manchester','Great Britain','Great Britain','Europe'),
+	('22','2345','Tottenham','London','Great Britain','Great Britain','Europe'),
+	('33','3456','Carlsyle','Edinburgh','Scotland','Scotland','Europe'),
+	('44','4567','Abaroska','Dakota','California','United States of America','America'),
+	('55','5678','Tianmenn','Beijing','Wuhan','China','Asia');
 
 
+CREATE TABLE factsales(
+    pid VARCHAR(30) REFERENCES product(pid),
+    sid VARCHAR(30) REFERENCES store(sid),
+    tid VARCHAR(30) REFERENCES timetable(tid),
+    units_sold NUMERIC(30),
+    total_sales NUMERIC(30)
+);
+INSERT INTO factsales(pid, sid, tid, units_sold, total_sales)
+VALUES
+    ('1','11','12',1000,20000),
+    ('2','11','34',2000,40000),
+    ('3','22','56',3000,60000),
+    ('4','33','78',4000,70000),
+    ('5','44','90',5000,90000);

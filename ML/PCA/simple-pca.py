f6a6380ef104701c46c6b95fad45aca2ed8a4572
AnupJoseph <anup20joseph@gmail.com>
2020-03-18 18:25:57 +0530

    Added scikit verification for PCA

--- ML/PCA/simple-pca.py
+++ ML/PCA/simple-pca.py
@@ -1,26 +1,46 @@
+print('*'*15+'Plain numpy implementation of PCA'+'*'*15)
 #Written using only numpy
 from numpy import array,mean,cov
 from numpy.linalg import eig
 
 #define matrix
 A = array([[1,2],[3,4],[5,6]])
-print(A)
+print("Initial Martix = {}".format(A))
 
 #Calculate mean
 M = mean(A.T,axis = 1)
-print(M)
+print("Mean of the matrix = {}".format(M))
 
 #Scale columns by subtracting column means
 C = A - M
-print(C)
+print("Column scaling applied over the matrix = {}".format(C))
 
 #Calculate covariance
 V = cov(C.T)
-print(V)
+print("Covariance of the matrix = {}".format(V))
 
 values,vectors = eig(V)
-print(vectors)
-print(values)
+print("Eigen Vectors = {}".format(vectors))
+print("Eigen values = {}".format(values))
 
 P = vectors.T.dot(C.T)
-print(P.T)
+print("Matrix after applying PCA = {}".format(P.T))
+
+#Scikit learn Verifcation of the above result
+print('*'*15+'Verification of the above result'+'*'*15)
+
+from numpy import array
+from sklearn.decomposition import PCA
+# define a matrix
+A = array([[1, 2], [3, 4], [5, 6]])
+print(A)
+# create the PCA instance
+pca = PCA(2)
+# fit on data
+pca.fit(A)
+# access values and vectors
+print(pca.components_)
+print(pca.explained_variance_)
+# transform data
+B = pca.transform(A)
+print(B)

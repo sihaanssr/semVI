ddee607bd5e09305a2256b67adf049e15f0b9222
AnupJoseph <anup20joseph@gmail.com>
2020-03-19 12:26:15 +0530

    Restructured

--- ML/PCA/simple-pca.py
+++ ML/PCA/simple-pca.py
@@ -2,32 +2,34 @@ print('*'*15+'Plain numpy implementation of PCA'+'*'*15)
 #Written using only numpy
 from numpy import array,mean,cov
 from numpy.linalg import eig
+from sklearn.decomposition._pca import PCA
 
 #define matrix
+print("\n")
 A = array([[1,2],[3,4],[5,6]])
-print("Initial Martix = {}".format(A))
+print("Initial Martix = {} \n".format(A))
 
 #Calculate mean
 M = mean(A.T,axis = 1)
-print("Mean of the matrix = {}".format(M))
+print("Mean of the matrix = {} \n".format(M))
 
 #Scale columns by subtracting column means
 C = A - M
-print("Column scaling applied over the matrix = {}".format(C))
+print("Column scaling applied over the matrix = {} \n".format(C))
 
 #Calculate covariance
 V = cov(C.T)
-print("Covariance of the matrix = {}".format(V))
+print("Covariance of the matrix = {} \n".format(V))
 
 values,vectors = eig(V)
-print("Eigen Vectors = {}".format(vectors))
-print("Eigen values = {}".format(values))
+print("Eigen Vectors = {} \n".format(vectors))
+print("Eigen values = {} \n".format(values))
 
 P = vectors.T.dot(C.T)
-print("Matrix after applying PCA = {}".format(P.T))
+print("Matrix after applying PCA = {} \n".format(P.T))
 
 #Scikit learn Verifcation of the above result
-print('*'*15+'Verification of the above result'+'*'*15)
+print('*'*15+'Verification of the above result using sklearn'+'*'*15)
 
 from numpy import array
 from sklearn.decomposition import PCA
